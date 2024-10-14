019dbba856ead35a38cdca41c0b4baa319719269
Thomas Newhouse <thomashenryn@gmail.com>
2024-10-14 12:11:30 +0100

    Add project files.

---
+++ CustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Rza_website.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Rza_website.Services
+{
+    public class CustomerService
+
+    {
+        private readonly TlS2302280RzaContext _context;
+        public CustomerService(TlS2302280RzaContext context)
+        {
+
+        }
+    }
+}
---
+++ Rza_website/Models/TlS2302280RzaContext.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace Rza_website.Models;
+
+public partial class TlS2302280RzaContext : DbContext
+{
+    public TlS2302280RzaContext()
+    {
+    }
+
+    public TlS2302280RzaContext(DbContextOptions<TlS2302280RzaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Room> Rooms { get; set; }
+
+    public virtual DbSet<Roombooking> Roombookings { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    => optionsBuilder.UseMySql("name=MySqlConnection", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PRIMARY");
+
+            entity.ToTable("customers");
+
+            entity.HasIndex(e => e.Email, "email").IsUnique();
+
+            entity.HasIndex(e => e.PhoneNumber, "phoneNumber").IsUnique();
+
+            entity.HasIndex(e => e.Username, "username").IsUnique();
+
+            entity.Property(e => e.CustomerId)
+                .ValueGeneratedNever()
+                .HasColumnName("customerID");
+            entity.Property(e => e.DateOfBirth).HasColumnName("dateOfBirth");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(20)
+                .HasColumnName("firstName");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(20)
+                .HasColumnName("lastName");
+            entity.Property(e => e.LoyaltyPoints).HasColumnName("loyaltyPoints");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(11)
+                .IsFixedLength()
+                .HasColumnName("phoneNumber");
+            entity.Property(e => e.Postcode)
+                .HasMaxLength(8)
+                .HasColumnName("postcode");
+            entity.Property(e => e.Username)
+                .HasMaxLength(20)
+                .HasColumnName("username");
+        });
+
+        modelBuilder.Entity<Room>(entity =>
+        {
+            entity.HasKey(e => e.RoomNumber).HasName("PRIMARY");
+
+            entity.ToTable("rooms");
+
+            entity.Property(e => e.RoomNumber)
+                .ValueGeneratedNever()
+                .HasColumnName("roomNumber");
+            entity.Property(e => e.Capacity).HasColumnName("capacity");
+            entity.Property(e => e.RoomType)
+                .HasMaxLength(20)
+                .HasColumnName("roomType");
+        });
+
+        modelBuilder.Entity<Roombooking>(entity =>
+        {
+            entity.HasKey(e => new { e.CustomerId, e.RoomNumber, e.StartDate })
+                .HasName("PRIMARY")
+                .HasAnnotation("MySql:IndexPrefixLength", new[] { 0, 0, 0 });
+
+            entity.ToTable("roombookings");
+
+            entity.HasIndex(e => e.RoomNumber, "roomNumber");
+
+            entity.Property(e => e.CustomerId).HasColumnName("customerId");
+            entity.Property(e => e.RoomNumber).HasColumnName("roomNumber");
+            entity.Property(e => e.StartDate).HasColumnName("startDate");
+            entity.Property(e => e.EndDate).HasColumnName("endDate");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Roombookings)
+                .HasForeignKey(d => d.CustomerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("roombookings_ibfk_1");
+
+            entity.HasOne(d => d.RoomNumberNavigation).WithMany(p => p.Roombookings)
+                .HasForeignKey(d => d.RoomNumber)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("roombookings_ibfk_2");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
